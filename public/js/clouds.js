12bb0afb71951bb99075fd4f2fef08c6cccbeb35
Wei Hsu <yayitswei@gmail.com>
2013-07-21 02:40:16 -0700

    first

---
+++ public/js/clouds.js
@@ -0,0 +1,51 @@
+var currentIndex = 0;
+var clouds =
+[
+{"name": "calistoga",
+  "specie": "specie1"},
+{"name": "kauai",
+  "specie": "specie2"},
+  ];
+
+
+$(document).ready(function() {
+  loadCloud(currentIndex);
+  for (i in clouds) {
+    $('#cloudthumbs').append("<li data-index=" + i + "><img class='navcloud' src='/images/"+ clouds[i].name + "-thumb.png'></li>");
+  }
+  $("#cloudthumbs li").click(goToCloud);
+});
+
+function loadCloud(index) {
+  cloudbg = $('#cloudbg');
+  cloudbg.fadeOut('slow', function() {
+    cloudbg.css("background-image","url(/images/" + clouds[index].name + ".png)");
+    cloudbg.fadeIn('slow');
+    $('#cloudname').html(clouds[index].genus + " " + clouds[index].specie);
+  });
+
+}
+
+function goPrevious() {
+  if (currentIndex==0) {
+    currentIndex=clouds.length-1;
+  } else {
+    currentIndex=currentIndex-1;
+  }
+  loadCloud(currentIndex);
+}
+
+function goNext() {
+  if (currentIndex==clouds.length-1) {
+    currentIndex=0;
+  } else {
+    currentIndex=currentIndex+1;
+  }
+  loadCloud(currentIndex);
+}
+
+function goToCloud() {
+  menuItem = $(this);
+  index = menuItem.data("index");
+  loadCloud(index);
+}
